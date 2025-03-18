bf4ba96ed0c54ac67ebf17e40a5189f754228560
ritananashi <deizu573@gmail.com>
2025-03-18 15:21:30 +0900

    fix: tailwind.config.jsのいらない記述を削除

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,11 +1,9 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,html}'
   ],
   theme: {
     extend: {
