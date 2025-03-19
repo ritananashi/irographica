f1eb9fba7c45bc799aa0205bdd6245a56b0c5723
ritananashi <deizu573@gmail.com>
2025-03-19 16:48:57 +0900

    add: 新規登録時に入力するカラムとバリデージョンの追加 #13

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, uniqueness: true, length: { maximum: 10 }
+  validates :account, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\z/ }, length: { minimum: 4, maximum: 20 }
+
+  def to_param
+    account
+  end
 end
