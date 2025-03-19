51432f9b6c14f505d9d63b64d7f0332e9c646a11
ritananashi <deizu573@gmail.com>
2025-03-19 12:25:27 +0900

    add: ユーザーモデル作成 #13

---
+++ db/migrate/20250319031639_add_name_to_users.rb
@@ -0,0 +1,6 @@
+class AddNameToUsers < ActiveRecord::Migration[7.2]
+  def change
+    add_column :users, :name, :string, null: false
+    add_column :users, :account, :string, null: false
+  end
+end
