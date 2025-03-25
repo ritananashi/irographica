53d74ae5e7a27e8c3e5c623adcd826e168e81d0d
ritananashi <deizu573@gmail.com>
2025-03-25 14:29:20 +0900

    update: brandモデルのバリデーションを変更

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,5 +1,7 @@
 class Brand < ApplicationRecord
   validates :name, presence: true
+  validates :official_url, format: { with: /\A[a-zA-Z]+\z/, message: "不正なURLです" }, allow_blank: true
+  validates :official_shopping_url, format: { with: /\A[a-zA-Z]+\z/, message: "不正なURLです" }, allow_blank: true
 
   has_many :products
 end
