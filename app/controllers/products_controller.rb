24b56a5dbf3d1a17ca8b6f55df0e0b1d16ccadec
ritananashi <deizu573@gmail.com>
2025-03-25 14:20:30 +0900

    add: newとcreateアクションを追加 #32

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,34 @@
 class ProductsController < ApplicationController
   def index
-    @Products = Product.includes(:brand).all
+    @products = Product.includes(:brand).all
   end
 
   def show
     @product = Product.find_by(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    brand_name = params[:product][:brand_name]
+    brand = Brand.find_or_create_by!(name: brand_name)
+    @product.brand_id = brand.id
+
+    if @product.save
+      flash[:notice] = t('product.new.notice')
+      redirect_to products_path
+    else
+      flash.now[:alert] = t('product.new.alert')
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :brand_id, :category_id)
   end
 end
