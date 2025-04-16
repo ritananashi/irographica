9cb2dc2516ee7cad42111d19cf48208a769192a6
ritananashi <deizu573@gmail.com>
2025-04-16 12:57:26 +0900

    add: アソシエーションを追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :product
   has_many_attached :images
+  has_many :likes, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 20 }
   validates :body, presence: true, length: { maximum: 2000 }
