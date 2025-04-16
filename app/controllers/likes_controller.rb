5c22dc354579df106a28a9c9e9232e8bf93265ea
ritananashi <deizu573@gmail.com>
2025-04-16 15:33:43 +0900

    createアクションto とdestroyアクションを定義 #105

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,15 @@
 class LikesController < ApplicationController
+  def create
+    review = Review.find(params[:review_id])
+    like = current_user.likes.new(review_id: review.id)
+    like.save!
+    redirect_to request.referer, notice: "いいねしました"
+  end
+
+  def destroy
+    review = Review.find(params[:review_id])
+    like = current_user.likes.find_by(review_id: review.id)
+    like.destroy!
+    redirect_to request.referer, notice: "いいねを取り消しました"
+  end
 end
