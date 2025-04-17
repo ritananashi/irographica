b3ea16c72a4175621f4eaa3b150ec82e8cd84fbc
ritananashi <deizu573@gmail.com>
2025-04-17 17:03:26 +0900

    update: ajax対応のためredirectの記述を削除 #106

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,14 +2,12 @@ class BookmarksController < ApplicationController
   before_action :redirect_root, only: [:create, :destroy]
 
   def create
-    review = Review.find(params[:review_id])
-    current_user.bookmark(review)
-    redirect_to request.referer, notice: "ブックマークしました"
+    @review = Review.find(params[:review_id])
+    current_user.bookmark(@review)
   end
 
   def destroy
-    review = current_user.bookmarks.find(params[:id]).review
-    current_user.unbookmark(review)
-    redirect_to request.referer, notice: "ブックマークを取り消しました", status: :see_other
+    @review = current_user.bookmarks.find(params[:id]).review
+    current_user.unbookmark(@review)
   end
 end
