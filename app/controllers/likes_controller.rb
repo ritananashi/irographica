e150725269503956f67a133dc811ed963a6ecd98
ritananashi <deizu573@gmail.com>
2025-04-17 12:46:41 +0900

    add: 未ログイン時にいいねボタンをクリックするとログイン画面に遷移するように設定 #105

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,6 @@
 class LikesController < ApplicationController
+  before_action :redirect_root, only: [:create, :destroy]
+
   def create
     @review = Review.find(params[:review_id])
     @like = current_user.likes.new(review_id: @review.id)
