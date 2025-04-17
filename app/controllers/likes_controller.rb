1ffd0f608a9456e4573df1d0c12322f713747759
ritananashi <deizu573@gmail.com>
2025-04-17 12:34:53 +0900

    update: いいね機能のAjax対応のため不要な記述を削除 #105

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,13 @@
 class LikesController < ApplicationController
   def create
-    review = Review.find(params[:review_id])
-    like = current_user.likes.new(review_id: review.id)
-    like.save!
-    redirect_to request.referer, notice: "いいねしました"
+    @review = Review.find(params[:review_id])
+    @like = current_user.likes.new(review_id: @review.id)
+    @like.save!
   end
 
   def destroy
-    review = Review.find(params[:review_id])
-    like = current_user.likes.find_by(review_id: review.id)
-    like.destroy!
-    redirect_to request.referer, notice: "いいねを取り消しました"
+    @review = Review.find(params[:review_id])
+    @like = current_user.likes.find_by(review_id: @review.id)
+    @like.destroy!
   end
 end
