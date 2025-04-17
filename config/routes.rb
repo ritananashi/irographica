5c6dd6bca28eb9111a3e29e0baac315b714fae58
ritananashi <deizu573@gmail.com>
2025-04-17 14:59:35 +0900

    add: ルーティングを設定 #106

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,12 @@ Rails.application.routes.draw do
   resources :reviews, only: %i[index show new create edit update destroy] do
     resources :attachments, controller: "review/attachments", only: :destroy
     resource :like, only: %i[create destroy]
-    get :search, on: :collection
+    collection do
+      get :search
+      get :bookmarks
+    end
   end
+  resources :bookmarks, only: %i[create destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
