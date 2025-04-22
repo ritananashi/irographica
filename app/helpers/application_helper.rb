ef8871b5d717a0452b89a5242d575717a37d305d
ritananashi <deizu573@gmail.com>
2025-04-22 16:43:54 +0900

    update: 各ヘルパーメソッドを移動したので元の記述を削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def post_review_url(review)
-    "https://irographica.com/reviews/#{review.id}"
-  end
-
-  def post_profile_url(user)
-    "https://irographica.com/users/#{user.account}"
-  end
 end
