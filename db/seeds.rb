0202540265f68b226e8c49478342e91d3b5d6cab
ritananashi <deizu573@gmail.com>
2025-04-23 14:40:41 +0900

    add: product.csvを読み込む記述を追加 #109

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,22 @@
 #   end
 require "csv"
 
-CSV.foreach('db/csv/brands.csv', headers: true) do |row|
-  Brand.find_or_create_by!(name: row['name']) do |brand|
-    brand.name = row['name']
-    brand.official_url = row['official_url']
-    brand.official_shopping_url = row['official_shopping_url']
+# Brand登録用
+# CSV.foreach('db/csv/brands.csv', headers: true) do |row|
+#   Brand.find_or_create_by!(name: row['name']) do |brand|
+#     brand.name = row['name']
+#     brand.official_url = row['official_url']
+#     brand.official_shopping_url = row['official_shopping_url']
+#   end
+# end
+
+# Product登録用
+CSV.foreach('db/csv/products.csv', headers: true) do |row|
+  brand = Brand.find_by(name: row['brand_name'])
+  category = Category.find_by(color: row['category'])
+  Product.find_or_create_by!(name: row['name']) do |product|
+    product.name = row['name']
+    product.brand_id = brand.id
+    product.category_id = category.id
   end
 end
