4cd8e0312ad28e4c296f8b9b574db7b331804a93
ritananashi <deizu573@gmail.com>
2025-05-01 12:19:12 +0900

    update: Categoryをセレクトボックスで選択できるように設定。画面表示がインク名になるように変更 #138

--- app/dashboards/product_dashboard.rb
+++ app/dashboards/product_dashboard.rb
@@ -9,9 +9,15 @@ class ProductDashboard < Administrate::BaseDashboard
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
     id: Field::Number,
-    brand: Field::BelongsTo,
-    category_id: Field::Number,
     name: Field::String,
+    brand: Field::BelongsTo,
+    category_id: Field::Select.with_options(
+      collection: Category.all.collect { |c| [ "#{c.color}", c.id ] },
+      include_blank: true,
+      getter: -> (field) {
+        field.resource.color
+      }
+    ),
     reviews: Field::HasMany,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
@@ -24,17 +30,17 @@ class ProductDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
     id
+    name
     brand
     category_id
-    name
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
     id
-    brand
     name
+    brand
     category_id
     reviews
     created_at
@@ -45,9 +51,9 @@ class ProductDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
+    name
     brand
     category_id
-    name
     reviews
   ].freeze
 
@@ -66,7 +72,7 @@ class ProductDashboard < Administrate::BaseDashboard
   # Overwrite this method to customize how products are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(product)
-  #   "#{product.id}"
-  # end
+  def display_resource(product)
+    "#{product.name}"
+  end
 end
