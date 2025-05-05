06ad3bf1986ce100d29983ce86a2d6eed7603d46
ritananashi <deizu573@gmail.com>
2025-05-05 17:43:20 +0900

    update: オートコンプリート用のビューでヘッダーが表示されないように設定 #110

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_header
 
   protected
 
@@ -12,5 +13,9 @@ class ApplicationController < ActionController::Base
 
   def redirect_root
     redirect_to new_user_session_path, alert: "ログインしてください" unless user_signed_in?
+  end
+
+  def set_header
+    @show_header = true
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :hide_header, only: :autocomplete
   def index
     if params[:q].blank?
       @q = params[:q]
@@ -67,5 +68,9 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:name, :brand_id, :category_id)
+  end
+
+  def hide_header
+    @show_header = false
   end
 end
