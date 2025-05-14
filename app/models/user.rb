7813995c2ba910e8bb8e28b63415733d3f90fc58
ritananashi <deizu573@gmail.com>
2025-05-14 16:47:06 +0900

    add: フォローフォロワー機能用のアソシエーションとメソッドを定義 #112

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,14 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   has_many :bookmark_reviews, through: :bookmarks, source: :review
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   has_one_attached :avatar
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 10 }
@@ -33,5 +41,17 @@ class User < ApplicationRecord
 
   def bookmarked_by?(review)
     bookmark_reviews.include?(review)
+  end
+
+  def follow(other_user)
+    following << other_user unless self == other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
