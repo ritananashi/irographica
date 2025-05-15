7b32dfa9ce49059d72b71ed9ad312ae836493113
ritananashi <deizu573@gmail.com>
2025-05-15 16:49:46 +0900

    fix: 余計な空白を削除 #112

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :relationships,       only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   resources :products, only: %i[index show new create] do
     collection do
