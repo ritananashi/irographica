e76057f4d572d21c4c2fbab3955468e6385d459e
ritananashi <deizu573@gmail.com>
2025-05-15 16:54:35 +0900

    fix: 他のユーザープロフィールに遷移後、自分のマイページに戻ったときにモーダルがチラつく問題を修正 #112

--- app/javascript/controllers/remote_modal_controller.js
+++ app/javascript/controllers/remote_modal_controller.js
@@ -2,7 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="remote-modal"
 export default class extends Controller {
+  static targets = ["frame"]
+
   connect() {
-    this.element.showModal()
+    this.element.showModal();
+  }
+
+  close() {
+    this.element.close();
+    this.element.remove();
   }
 }
