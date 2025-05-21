4d41e79d0377d3917acd6a43f8dfdb84893ad67f
ritananashi <deizu573@gmail.com>
2025-05-21 16:14:44 +0900

    add: バリデーションの追加 #113

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,10 @@
 class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :notifable, polymorphic: true
+
+  validates :user_id, presence: true
+  validates :notifable_id, presence: true
+  validates :notifable_type, presence: true
+
+  scope :unread, -> { where(read: false) }
 end
