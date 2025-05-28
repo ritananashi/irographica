b5a16d8819abf326232f331f51b30c2602fa7fb0
ritananashi <deizu573@gmail.com>
2025-05-28 17:00:42 +0900

    add: OGP画像をCDNで配信してみる

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,32 @@ Rails.application.routes.draw do
     resource :like, only: %i[create destroy]
     get :search, on: :collection
   end
+
+  direct :cdn_image do |model, options|
+    expires_in = options.delete(:expires_in) { ActiveStorage.urls_expire_in }
+
+    if model.respond_to?(:signed_id)
+      route_for(
+        :rails_service_blob_proxy,
+        model.signed_id(expires_in: expires_in),
+        model.filename,
+        options.merge(host: ENV["CDN_HOST"])
+      )
+    else
+      signed_blob_id = model.blob.signed_id(expires_in: expires_in)
+      variation_key  = model.variation.key
+      filename       = model.blob.filename
+
+      route_for(
+        :rails_blob_representation_proxy,
+        signed_blob_id,
+        variation_key,
+        filename,
+        options.merge(host: ENV["CDN_HOST"])
+      )
+    end
+  end
+
   resources :bookmarks, only: %i[create destroy]
   resources :notifications, only: :index
 
