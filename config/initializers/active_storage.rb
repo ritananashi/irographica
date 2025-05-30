042a18981a632d2b0fc6300a8162333cb0316a5b
ritananashi <deizu573@gmail.com>
2025-05-30 13:49:09 +0900

    fix: やっぱり元のコードであってたので修正

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,2 +1,2 @@
 # S3の画像をデフォルトでプロキシモードで使用する用の設定
-Rails.application.config.active_storage.resolve_model_to_route = :rails_storage_proxy
+Rails.application.config.active_storage.resolve_model_to_route = :cdn_image
