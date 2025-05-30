f8e98b05520d97202f0596f127c578318736b3b3
ritananashi <deizu573@gmail.com>
2025-05-30 12:14:15 +0900

    fix: プロキシモードの設定を間違えていたので修正

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,2 +1,2 @@
-# S3の画像をデフォルトでCDN配信する設定
-Rails.application.config.active_storage.resolve_model_to_route = :cdn_image
+# S3の画像をデフォルトでプロキシモードで使用する用の設定
+Rails.application.config.active_storage.resolve_model_to_route = :rails_storage_proxy
