ba23ed7a6aa04cf2e715280afeb9d7fa2f4a9715
ritananashi <deizu573@gmail.com>
2025-06-05 16:12:08 +0900

    add: Xログインに必要なカラムを追加 #117

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_16_044708) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_05_065833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -140,10 +140,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_16_044708) do
     t.string "instagram_account"
     t.string "youtube_account"
     t.boolean "admin", default: false
+    t.string "provider"
+    t.string "uid"
     t.index ["account"], name: "index_users_on_account", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
