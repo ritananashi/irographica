eea733fafc45663e8ff51712673422e158fcd007
ritananashi <deizu573@gmail.com>
2025-06-05 16:39:20 +0900

    add: deviseでXログインを使う設定を追加 #117

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :twitter, ENV["TWITTER_CLIENT_ID"], ENV["TWITTER_CLIENT_SECRET"], callback_path: '/auth/twitter2/callback', scope: "users.read"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
