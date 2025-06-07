89784a2c8d9013eb56785f404b9311cac225e43b
ritananashi <deizu573@gmail.com>
2025-06-07 13:05:13 +0900

    add: letter_opener_web用のルーティングを設定 #118

--- config/routes.rb
+++ config/routes.rb
@@ -76,6 +76,8 @@ Rails.application.routes.draw do
 
     root to: "reviews#index"
   end
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
