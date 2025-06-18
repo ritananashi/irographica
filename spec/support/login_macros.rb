ed5ff270632e30c4bb98f6383c8c09af7d1ecd03
ritananashi <deizu573@gmail.com>
2025-06-18 16:35:54 +0900

    fix: いらなかったので削除 #148

--- spec/support/login_macros.rb
+++
@@ -1,8 +0,0 @@
-module LoginMacros
-  def login(user)
-    visit new_user_session_path
-    fill_in 'Eメール', with: user.email
-    fill_in 'パスワード', with: user.password
-    click_button 'ログイン'
-  end
-end
