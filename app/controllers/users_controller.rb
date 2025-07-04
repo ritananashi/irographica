76864d5b6f6575454fef210763bc9473892c4c83
ritananashi <deizu573@gmail.com>
2025-07-04 13:09:33 +0900

    add: コレクション・ブックマーク・フォローフォロワー一覧をページネーション対応 #248

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,25 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(account: params[:id])
-    @reviews = @user.reviews.order(created_at: "DESC")
+    @pagy, @reviews = pagy(@user.reviews.order(created_at: :desc), limit: 10)
   end
 
   def bookmarks
     @user = User.find_by(account: params[:id])
-    @bookmark_reviews = @user.bookmark_reviews.includes(:user).order(created_at: "DESC")
+    @pagy, @bookmark_reviews = pagy(@user.bookmark_reviews.includes(:user).order(created_at: :desc), limit: 10)
   end
 
   def following
     @title = "フォロー一覧"
     @user = User.find_by(account: params[:id])
-    @users = @user.following
+    @pagy, @users = pagy(@user.following, limit: 5)
     render "show_follow"
   end
 
   def followers
     @title = "フォロワー一覧"
     @user = User.find_by(account: params[:id])
-    @users = @user.followers
+    @pagy, @users = pagy(@user.followers, limit: 5)
     render "show_follow"
   end
 end
