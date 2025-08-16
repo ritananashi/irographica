d518fe134361bfaf4397369d4c7878f3a11a26c5
ritananashi <deizu573@gmail.com>
2025-08-16 18:06:01 +0900

    add: 楽天API検索用のジョブ作成
    楽天API用のジョブを作成しました。
    インク登録時に非同期に登録されたインクの名前で検索を行い、取得した結果をデータベースに保存します。
    検索は商品価格ナビ製品検索APIで行い、結果がなければ楽天商品検索APIで追加の検索を行います。
    不審なショップに直接繋がるのをできるだけ避けるために、なるべくショップではなく商品価格ナビに繋がるようにしています。
    
    #267

--- app/jobs/rakuten_data_fetch_job.rb
+++ app/jobs/rakuten_data_fetch_job.rb
@@ -1,7 +1,24 @@
 class RakutenDataFetchJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(product_id)
+    product = Product.find(product_id)
+
+    key = "インク #{product.name}"
+    results_list = RakutenWebService::Ichiba::Product.search(keyword: key, genreId: '215783')
+    unless results_list.any?
+      sleep(1) # 制限回避用
+      results_list = RakutenWebService::Ichiba::Item.search(keyword: key, genreId: '215783')
+    end
+
+    sleep(1) # 制限回避用
+
+    if results_list.any?
+      result = results_list.first
+      product.update!(
+        productUrl: result['itemUrl'] || result['productUrlPC'],
+        imageUrl: result['mediumImageUrl'] || result['mediumImageUrls'][0]
+      )
+    end
   end
 end
