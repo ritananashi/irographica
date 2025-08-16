c56a62bb2df9ae2ba934adf14eefe1dd54e56487
ritananashi <deizu573@gmail.com>
2025-08-16 16:31:07 +0900

    add: 楽天APIの設定ファイルを作成
    楽天APIを使用するための設定ファイルを作成しました。
    
    #267

---
+++ config/initializers/rakuten.rb
@@ -0,0 +1,17 @@
+RakutenWebService.configure do |c|
+  # (必須) アプリケーションID
+  c.application_id = Rails.env.production? ? ENV["RAKUTEN_APPLICATION_ID"] : Rails.application.credentials.dig(:rakuten, :RAKUTEN_APPLICATION_ID)
+
+  # (任意) 楽天アフィリエイトID
+  c.affiliate_id = Rails.env.production? ? ENV['RAKUTEN_AFFILIATE_ID'] : Rails.application.credentials.dig(:rakuten, :RAKUTEN_AFFILIATE_ID)
+
+  # (任意) リクエストのリトライ回数
+  # 一定期間の間のリクエスト数が制限を超えた時、APIはリクエスト過多のエラーを返す。
+  # その後、クライアントは少し間を空けた後に同じリクエストを再度送る。
+  c.max_retries = 3 # default: 5
+
+  # (任意) デバッグモード
+  # trueの時、クライアントはすべてのHTTPリクエストとレスポンスを
+  # 標準エラー出力に流す。
+  c.debug = true # default: false
+end
