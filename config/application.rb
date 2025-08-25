ee99b379b805131ff04319376da0f8d345b676d5
ritananashi <deizu573@gmail.com>
2025-08-25 17:35:02 +0900

    add: Administrateのカスタムフィールド生成で生成されたファイルを自動読み込みパスに追加
    カスタムフィールドが読み込まれなかったので生成されたファイルを自動読み込みパスに追加しました。
    
    #267

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Myapp
     config.load_defaults 7.2
     config.active_storage.variant_processor = :mini_magick
 
+    config.autoload_paths << "#{root}/app/fields"
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
