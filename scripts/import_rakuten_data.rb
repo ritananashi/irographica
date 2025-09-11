ca1424c3402375fcbff20a4a42d9474574230920
ritananashi <deizu573@gmail.com>
2025-09-11 18:41:30 +0900

    improve: 楽天の情報取得を非同期処理に変更
    同期処理だと終了まで時間がかかるので、非同期処理で行うように変更しました。
    
    #267

--- scripts/import_rakuten_data.rb
+++ scripts/import_rakuten_data.rb
@@ -1,17 +1,4 @@
 product_all = Product.where.not("name LIKE ?", "%オリジナル%")
 
-product_all.each do |product|
-  search_result = RakutenWebService::Ichiba::Product.search(keyword: "インク　#{product.name}", genreId: '215783')
-  unless search_result.any?
-    sleep(1)
-    search_result = RakutenWebService::Ichiba::Item.search(keyword: "インク　#{product.name}", genreId: '215783')
-  end
-  sleep(1)
-  if search_result.any?
-    result = search_result.first
-    product.update!(
-      productUrl: result['itemUrl'] || result['productUrlPC'],
-      imageUrl: result['mediumImageUrl'] || result['mediumImageUrls'][0]
-    )
-  end
-end
+import_rakuten_data = product_all.all.map { |product| RakutenDataFetchJob.new(product.id) }
+ActiveJob.perform_all_later(import_rakuten_data)
