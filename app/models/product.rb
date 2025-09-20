7d2f068e69160c39b4fd28a1153881ec9e4db8be
ritananashi <deizu573@gmail.com>
2025-09-20 18:38:31 +0900

    fix: nameカラムの一意制約を修正
    uniqness: trueだと別ブランドの同名インクを登録することができないので、
    scopeで同ブランドの同名インクを登録できないように変更しました。
    
    #142

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   include SortStatus
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { scope: :brand }
   validates :category_id, presence: true
 
   belongs_to :brand
