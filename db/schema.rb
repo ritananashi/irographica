7531efdd23ddeefbf5a5ca3bdb6082132db71754
ritananashi <deizu573@gmail.com>
2025-09-22 19:20:39 +0900

    add: nameカラムのインデックスを削除
    nameカラムのインデックスを削除しました。
    この後、nameとbrand_idの複合インデックスにして一意制約をつけます。
    
    #142

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_16_085424) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_22_095246) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -101,7 +101,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_16_085424) do
     t.string "imageUrl"
     t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["category_id"], name: "index_products_on_category_id"
-    t.index ["name"], name: "index_products_on_name", unique: true
   end
 
   create_table "relationships", force: :cascade do |t|
