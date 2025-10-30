19ae1c69561cdded8ac37fc96664a95a0e796d10
ritananashi <deizu573@gmail.com>
2025-10-30 14:39:04 +0900

    fix: lintチェック修正
    lintに引っ掛かった部分を修正しました。
    
    #320

--- db/migrate/20251029092346_reset_images_count_on_review.rb
+++ db/migrate/20251029092346_reset_images_count_on_review.rb
@@ -1,6 +1,6 @@
 class ResetImagesCountOnReview < ActiveRecord::Migration[7.2]
   def up
-    Review.find_each{ |review| review.update_column(:images_count, review.images.count) }
+    Review.find_each { |review| review.update_column(:images_count, review.images.count) }
   end
 
   def down
