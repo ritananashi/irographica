c856940531f645cf55d595841a23e6ae38792c0a
ritananashi <deizu573@gmail.com>
2025-11-12 18:34:39 +0900

    add: ページネーション対応が漏れていた部分にページネーションを追加
    ページネーションが漏れていた部分にページネーション対応を追加しました。

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :redirect_root, only: :index
 
   def index
-    @notifications = current_user.notifications.includes(:notifable).order(created_at: :desc)
+    @pagy, @notifications = pagy(current_user.notifications.includes(:notifable).order(created_at: :desc), limit: 10)
 
     current_user.read_notification if @notifications.unread.present?
   end
